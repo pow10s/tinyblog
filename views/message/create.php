f27f5d8d85c8621e9dd94c3c67cdd102d036bd25
pretender <100sbs@gmail.com>
2015-03-21 12:02:20 +0200

    Added files and folders

---
+++ views/message/create.php
@@ -0,0 +1,6 @@
+<HTML>
+<form action="http://localhost/projs/tinyblog/views/message/create.php" method="POST">
+    <input type="text" name="nickname" size="100" maxlength="100">
+    <input type="text" name="message"
+</form>
+</HTML>
