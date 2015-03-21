0aabb01170ab743f27156dc24dcd5ad62cf22d6a
pretender <100sbs@gmail.com>
2015-03-21 14:06:17 +0200

    Refreshed create.php

--- views/message/create.php
+++ views/message/create.php
@@ -1,6 +1,23 @@
 <HTML>
-<form action="http://localhost/projs/tinyblog/views/message/create.php" method="POST">
-    <input type="text" name="nickname" size="100" maxlength="100">
-    <input type="text" name="message"
+<form method="POST">
+    <p>Input nickname: </p>
+    <input type="text" name="nickName">
+    <p>Write message: </p>
+    <p><textarea rows="10" cols="45" name="text"></textarea></p>
+    <input type=submit [name=input] value=Submit>
 </form>
 </HTML>
+<?php
+ini_set('display_errors','Off');
+if (empty($_POST['input'])) {
+    if (($_POST['nickName']=='') && isset($_POST['text'])) {
+        echo 'Please input NICKNAME';
+    }
+    elseif (isset($_POST['nickName']) && isset($_POST['text'])) {
+        echo $_POST['nickName'] . ': ' . $_POST['text'] ;
+
+    }
+}
+else {
+    echo 'Input data';
+}
