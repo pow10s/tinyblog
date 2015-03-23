17c700a7d08fa91f2c3c68c63c1357afbbf05a8e
stosdima <stosdima@gmail.com>
2015-03-23 14:50:10 +0200

    Changed files

--- views/message/create.php
+++ views/message/create.php
@@ -1,11 +1,11 @@
-<HTML>
+<html>
 <form method="POST">
     <p>Input nickname: </p>
     <input type="text" name="nickName">
     <p>Write message: </p>
     <p><textarea rows="10" cols="45" name="text"></textarea></p>
-    <input type=submit name=input value=Submit>
+    <input type="submit" name="btn_message" value=Submit>
 </form>
-</HTML>
+</html>
 <?php
 
