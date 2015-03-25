d73ec4e2b0bc41fc739e85e241bd4fa001daa147
stosdima <stosdima@gmail.com>
2015-03-25 20:39:00 +0200

    changed

--- libs/logger.php
+++ libs/logger.php
@@ -1,13 +1,13 @@
 <?php
 function logs($message, $pathToLog) {
-  file_put_contents($message, $pathToLog);
+    if (file_exists(PATH_TO_LOG)) {
 
-}
-if (file_exists(PATH_TO_LOG)) {
+        file_put_contents($message, $pathToLog);
+    }
+    else {
+        echo 'Error. File doesnt exist';
+    }
 
-    logs ('Test', PATH_TO_LOG);
-}
-else {
-    echo 'Error. File doesnt exist';
+
 }
 
