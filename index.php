e539921b741e33b052c22e072fd71b5ab5db302b
pretender <100sbs@gmail.com>
2015-03-28 21:37:46 +0200

    Write data in txt-file

--- index.php
+++ index.php
@@ -1,13 +1,14 @@
 <?php
-//include ('config.php');
+include ('config.php');
 //include ('views/message/create.php');
-  if ($_GET['action']=='register'){
-      include ('views/registration.php');
-      include ('libs/validation.php');
+$file = fopen('db/user.txt', 'a');
+  if ($_GET['action']=='register') {
+      include('views/registration.php');
+      include('libs/validation.php');
       if (isset($_POST['submit_btn'])) {
-          checkMail($_POST['e-mail']);
-          checkUser($_POST['username']);
-          checkPass($_POST['pass']);
-
+          if (checkMail($_POST['e-mail']) == 'TRUE' && checkUser($_POST['username']) == 'TRUE' && checkPass($_POST['pass']) == 'TRUE') {
+              fputs($file, $_POST['e-mail'] . ';' . $_POST['username'] . ';' . $_POST['pass'] . "\r\n");
+          }
+          fclose($file);
       }
   }
