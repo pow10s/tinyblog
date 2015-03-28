1b26685beedf760b3dd44cb766bce1179e260b9b
pretender <100sbs@gmail.com>
2015-03-28 21:55:00 +0200

    Changed files

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 <?php
 include ('config.php');
 //include ('views/message/create.php');
-$file = fopen('db/user.txt', 'a');
+
   if ($_GET['action']=='register') {
       include('views/registration.php');
       include('libs/validation.php');
       if (isset($_POST['submit_btn'])) {
-          if (checkMail($_POST['e-mail']) == 'TRUE' && checkUser($_POST['username']) == 'TRUE' && checkPass($_POST['pass']) == 'TRUE') {
-              fputs($file, $_POST['e-mail'] . ';' . $_POST['username'] . ';' . $_POST['pass'] . "\r\n");
+          if (checkMail($_POST['e-mail']) == true && checkUser($_POST['username']) == true && checkPass($_POST['pass']) == true) {
+              file_put_contents('db/user.txt', $_POST['e-mail'] . ';' . $_POST['username'] . ';' . $_POST['pass'] . "\r\n", FILE_APPEND);
           }
-          fclose($file);
+
       }
   }
