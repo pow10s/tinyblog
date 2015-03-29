3a0951d2abb487d9ec46002be36aa06ef3e1a786
pretender <100sbs@gmail.com>
2015-03-29 11:25:47 +0300

    search.php

--- libs/search.php
+++ libs/search.php
@@ -1,5 +1,5 @@
 <?php
-/*function searchByUserChunk($chunkOfName, $pathFile) {
+function searchByUserChunk($chunkOfName, $pathFile) {
     $search = $chunkOfName;
     $lines = file($pathFile);
     foreach ($lines as $num_line => $line_value) {
@@ -8,17 +8,4 @@
             return true;
     }
     return false;
-}*/
-
-/*$raw_query_string = 'act=search&sw=%E1%CC%D8%C2%CF%CD&where=news&cp=35&cp=2';
-preg_match ("/^(.+)(&cp\=\d+)+/", $raw_query_string, $query_string);
-echo "$query_string[1]\n"; // act=search&sw=%E1%CC%D8%C2%CF%CD&where=news&cp=35
-echo "$query_string[2]\n"; // &cp=2*/
-
-$text = FILE("user.txt");
-$search = "abra";
-for ($i = 0; $i < count($text); $i++){
-    if (!(stripos($text[$i],$search) === FALSE)) echo $text[$i]."<br>";
 }
-
-?>
