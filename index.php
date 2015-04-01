1d3b5f6f1a2dc648321a13a0d7fc5cd03b9fde13
stosdima <stosdima@gmail.com>
2015-04-01 17:26:04 +0300

    changed login.php/index.php

--- index.php
+++ index.php
@@ -28,11 +28,9 @@ if($_GET['action']=='login'){
     if (isset($_POST['enter_btn'])) {
       if (validationUserNamePass($_POST['loginUserName'], $_POST['loginPassword'], 'db/user.txt',"user")) {
           setcookie("user", $_POST['loginUserName']);
-          ob_start();
           echo 'Welcome, ' . $_COOKIE["user"];
           if (!isset($_COOKIE['user']) || $_COOKIE['user'] == $_POST['loginUserName']){
               include_once 'views/loginForm.php';
-              ob_end_clean();
               echo 'You are logged in to your account.';
           }
       }
--- libs/login.php
+++ libs/login.php
@@ -4,15 +4,17 @@ function validationUserNamePass($Name, $Password , $pathFile,$cookieName) {
     $searchPassword = $Password;
     $lines = file($pathFile);
     foreach ($lines as $num_line => $line_value) {
+        if(!empty($Name)){
         if (strpos($line_value, $searchName) !== false && strpos($line_value, $searchPassword) !== false) {
             $pieces = explode(";", $line_value);
             if($Name!==$pieces[1] && $Password!==$pieces[2]){
                     return false;
             }
-            return true;
 
         }
+        return true;
 
     }
 
+    }
 }
