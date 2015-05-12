7196562e24c89bc4f22bc2917f6ccb8d38d9d9da
pretender <100sbsh@gmail.com>
2015-05-12 21:48:41 +0300

    Added comments for code

--- db/findByLogin.php
+++ db/findByLogin.php
@@ -2,9 +2,13 @@
 function findByLogin($login)
 {
     $db = PDO('mysql:host=localhost;dbname=users', 'root', '');
+    //Це самий тупий метод в моєму житті
+    //Ти чомусь вигріб усіх користувачів(хоча тобі треба було 1го з логіном). Можна ж через sql "select * from users where login=$login"
     $stmt = $db->query('SELECT * from users');
-//Установка fetch mode
+
+
     $stmt->setFetchMode(PDO::FETCH_ASSOC);
+     //Далі по циклу ти їх проходиш
     while ($row = $stmt->fetch()) {
         echo $login;
 
