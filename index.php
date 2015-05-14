a574cc096f97d979cb31c6495bd853db32d0da85
stosdima <stosdima@gmail.com>
2015-05-14 21:31:47 +0300

    changed index.php

--- index.php
+++ index.php
@@ -2,14 +2,14 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 include ('config.php');
-
-if (isset($_GET['action'])=='register') {
-    include ('controllers/Register.php');
+if (isset($_GET['action'])) {
+    if ($_GET['action'] == 'register') {
+        include('controllers/Register.php');
+    }
+    if ($_GET['action'] == 'login') {
+        include('controllers/Login.php');
+    }
+    if ($_GET['action'] == 'search') {
+        include('controllers/Search.php');
+    }
 }
-if(isset($_GET['action'])=='login'){
-    include ('controllers/Login.php');
-}
-if (isset($_GET['action'])=='search') {
-    include ('controllers/Search.php');
-}
-
