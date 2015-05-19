f122f5363e6b0aae6e7677b98c97fede9d5be2f5
stosdima <stosdima@gmail.com>
2015-05-19 20:47:14 +0300

    added averagerArray.php

--- views/message.php
+++ views/message.php
@@ -5,15 +5,10 @@
     <p>Message:</p>
     <p><textarea name="message"></textarea></p>
     <?php
-    $db = new PDO("mysql:dbname=users;host=localhost", "root", "");
-    $stmt = $db->prepare("SELECT UserName,id FROM users");
-    $stmt->execute();
-    $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
     echo "<select name=\"users_list\">";
     echo "<option size =30 ></option>";
-    foreach ($result as $key => $value) {
-        echo "<option value='".$value['id']."'>".$value['UserName']."</option>";
-    }
+    include_once ('dropDownList.php');
+    dropDownList();
     echo "</select>";
     ?>
     <p><input type ="submit" name="send_btn" value="Send"></p>
