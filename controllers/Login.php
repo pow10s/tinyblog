1317533f1fd3dfd07f161918b1bb6ade19c35386
pretender <100sbsh@gmail.com>
2015-05-23 17:33:48 +0300

    Modified login

--- controllers/Login.php
+++ controllers/Login.php
@@ -1,15 +1,5 @@
 <?php
-<<<<<<< HEAD
-    if (isset($_POST['enter_btn'])) {
-        setcookie("user", $_POST['loginUserName']);
 
-        if (!isset($_COOKIE['user'])) {
-            include_once 'views/loginForm.php';
-            include 'db/selectFromTable.php';
-            selectFromTable('UserName', 'Password', $_POST['loginUserName'], $_POST['loginPassword']);
-        } elseif ( $_COOKIE['user'] == $_POST['loginUserName']) {
-            echo 'You are logged in you account';
-=======
 if (isset($_COOKIE['user'])) {
     echo "You are logged";
 }
@@ -20,7 +10,6 @@ else {
         if(selectFromTable($_POST['loginUserName'], $_POST['loginPassword'])){
             setcookie("user", $_POST['loginUserName']);
             echo "Welcome to the site";
->>>>>>> f122f5363e6b0aae6e7677b98c97fede9d5be2f5
         }
         else echo "User not found";
 
