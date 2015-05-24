cc3001b9ed08e48b8ddc245b0c69b75f0cc713db
stosdima <stosdima@gmail.com>
2015-05-24 23:04:19 +0300

    added check for messages

--- controllers/messageController.php
+++ controllers/messageController.php
@@ -2,10 +2,20 @@
 if (isset($_COOKIE['user'])) {
     include_once ('views/message.php');
     if (isset($_POST['send_btn'])) {
-        include_once('db/addToTableMessages.php');
-        include_once ('db/getUserIdByUserName.php');
-        $result = getUserIdByUserName ($_COOKIE['user']);
-        addToTableMessages($_POST['titleName'], $_POST['message'], $result, $_POST['users_list']);
+        if (!empty($_POST['titleName']) && !empty($_POST['message'])) {
+            include_once('db/addToTableMessages.php');
+            include_once('db/getUserIdByUserName.php');
+            $result = getUserIdByUserName($_COOKIE['user']);
+            if ($result!=$_POST['users_list']){
+            addToTableMessages($_POST['titleName'], $_POST['message'], $result, $_POST['users_list']);
+            }
+            else {
+                echo "You cant send message oneself";
+            }
+        }
+        else {
+            echo "Type Title or Message";
+        }
     }
 }
 else {
