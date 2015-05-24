d72dd7ffb6d3b2b5903abb6632547673139a0c8b
stosdima <stosdima@gmail.com>
2015-05-24 23:31:46 +0300

    changed viewMessage

--- controllers/viewMessagesController.php
+++ controllers/viewMessagesController.php
@@ -2,6 +2,8 @@
 include_once ('db/getUserIdByUserName.php');
 include_once ('libs/viewMessages.php');
 $result = viewMessage(getUserIdByUserName($_COOKIE['user']));
+echo "<table border = 1>". "<tr>"."<th>"."Title"."</th>"."</tr>"."<tr>"."<th>"."Message"."</th>"."</tr>";
 foreach($result as $key =>$value){
-    echo "<table border = 1>". "<tr>"."<th>"."Title"."</th>"."<td>".$value['Title']."</td>"."</tr>"."<tr>"."<th>"."Message"."</th>"."<td>".$value['Text']."</td>"."</tr>"."</table>";
+    echo "<tr>"."<td>".$value['Title']."</td>"."</tr>"."<tr>"."<td>".$value['Text']."</td>"."</tr>";
 }
+echo "</table>";
