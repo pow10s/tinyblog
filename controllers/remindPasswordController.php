d3d2286930ab03b53e0fb9628c984c7f84c91320
stosdima <stosdima@gmail.com>
2015-06-07 06:42:16 +0300

    Added 'Check forgoten password'

--- controllers/remindPasswordController.php
+++ controllers/remindPasswordController.php
@@ -4,9 +4,12 @@ if (isset($_COOKIE['user'])) {
 }
 else {
     include_once '../views/remindPassword.php';
-    if (isset($_POST['remind_btn'])){
-        include_once ('../models/reminder.php');
-        $object = new PasswordByEmail($_POST['remind_Email']);
-        echo "Your password: " . $object->PasswordByEmail();
+    if (isset($_POST['remind_btn'])) {
+        include_once('../libs/validation.php');
+        if (ValidationFields::checkMail($_POST['remind_Email'])) {
+            include_once('../models/reminder.php');
+            $object = new PasswordByEmail($_POST['remind_Email']);
+            echo "Password: " . $object->PasswordByEmail();
+        } else echo "Incorrect E-mail";
     }
 }
