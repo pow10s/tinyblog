58cc1447402f9a6316a8ace9b2f541220bbad3fe
stosdima <stosdima@gmail.com>
2015-10-19 01:22:06 +0300

    Mailer

--- controllers/registerController.php
+++ controllers/registerController.php
@@ -5,22 +5,30 @@ class RegisterController
     {
         $view = new \libs\View();
         $view->renderPartial('register');
-        if (isset($_POST['send_btn']))
-        {
+        if (isset($_POST['send_btn'])) {
             $hasher = new \libs\Hashing();
             $users = new \models\UserModel();
-
+            $mail = new PHPMailer();
             $hasher->setSalt('942c916c16bf1f03dc157290d30d6312');
             $result = $hasher->hash($_POST['password']);
-            $res =  $users->selectUser(array('password'=>$result),1);
-            $activation = $hasher->hash($_POST['user_name']);
+            $res = $users->selectUser(array('password' => $result), 1);
             if (isset($res[0][0]) != $result)
             {
-                $users->addUser(array('Email'=>$_POST['email'],'UserName'=>$_POST['user_name'], 'Password'=>$result,'activationCode'=>$activation));
+
+                $users->addUser(array('Email'=>$_POST['email'],'UserName'=>$_POST['user_name'], 'Password'=>$result));
+                $mail->From = "tinyblog.dev";
+                $mail->FromName = 'Anaconda';
+                $mail->addAddress($_POST['email'], $_POST['user_name']);
+                if (!$mail->send()) {
+                    echo "Mailer Error: " . $mail->ErrorInfo;
+                }else {
+                    echo "Message has been sent successfully";
+                   }
+               }
+
 
             }else{
                 echo 'exist in db';
             }
         }
     }
-}
