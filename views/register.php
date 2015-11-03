7860e7d7c1cd1524513e255fce05411c2965284e
stosdima <stosdima@gmail.com>
2015-11-03 17:57:00 +0000

    c9

---
+++ views/register.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+    <title>Tinyblog</title>
+    </head>
+<body>
+<form name = "register_form" method="post" id ="reg_form">
+    <h1>Tinyblog</h1>
+
+    <p>Input e-mail address:</p>
+    <input  type = "text" name="email" size = "20">
+
+    <p>Input username:</p>
+    <input  type = "text" name="user_name" size = "20">
+
+    <p> Input password:</p>
+    <input  type = "text" name="password" size = "20">
+    <p> <input type = "submit" name = "send_btn" value = "Send" id="processBtn"></p>
+    </form>
+</body>
