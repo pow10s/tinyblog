ffffa2d43c49acc3cb1caae8b340957523d41dd4
Dima Stos <Dima Stos>
2015-12-06 11:23:19 +0200

    Fome fixes

--- controllers/LoginController.php
+++ controllers/LoginController.php
@@ -4,7 +4,7 @@ class LoginController
 {
     public function actionLogin()
     {
-        ob_start();
+
         $logg = new \libs\LoggedUser();
         $view = new \libs\View();
         $selectLoginAndPass = new \models\UserModel();
@@ -21,15 +21,13 @@ class LoginController
                 try {
                     $userData = $selectLoginAndPass->selectUser(array('UserName', 'Password'), 'UserName = :name AND Password = :pass', array(':name' => $_POST['log_user_name'], ':pass' => $hash));
 
-
-
                         if ($userData[0]['UserName'] == $_POST['log_user_name'] && $userData[0]['Password'] == $hash) {
                             $cookie->create('UserName', $_POST['log_user_name'], 3600);
                             echo 'Thanks';
                         } else {
                             echo 'user doesnt exist';
                         }
-                    
+
                 } catch (Exception $e) {
 
                     echo $e->getMessage();
