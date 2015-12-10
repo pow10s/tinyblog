4470f590507769ab584b413a31c242cfe0362736
pretender <100sbsh@gmail.com>
2015-12-10 19:27:38 +0200

    Fixed merge

--- index.php
+++ index.php
@@ -1,10 +1,6 @@
 <?php
-<<<<<<< HEAD
-//ob_start();
-=======
-
->>>>>>> 5688f428c80e711d638b334a4605d22bd83d65b0
 require('config.php');
-require_once (__DIR__ . '/vendor/autoload.php');
+require_once(__DIR__ . '/vendor/autoload.php');
+
 $router = new libs\Router();
 $router->run();
