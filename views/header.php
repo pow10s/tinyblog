c3e811d664ec61c4041b6015ef61cf49dd582c1d
pretender <100sbs@gmail.com>
2015-12-10 20:00:59 +0200

    Fix

--- views/header.php
+++ views/header.php
@@ -2,11 +2,8 @@
     <li class="dropdown-top">
         <p>Menu</p>
         <ul class="dropdown-inside">
-            <?php
-            echo "<li>". "<a href =" . BASE_URL."/register/register>Register </a>" . "</li>";
-            echo "<li>". "<a href =" . BASE_URL."/login/login>Login </a>" . "</li>";
-            ?>
-
+            <li><a href =" <?php echo BASE_URL?>/register/register">Register </a></li>
+            <li><a href =" <?php echo BASE_URL?>/login/login">Login </a></li>
         </ul>
     </li>
     </ul>
